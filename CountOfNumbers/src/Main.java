7666399651193d95d7043cb669359c3cf3c67693
Rithanyaa <rithanyaa.virtue@gmail.com>
2026-03-21 19:04:54 +0530

    GCD of two num

--- CountOfNumbers/src/Main.java
+++ CountOfNumbers/src/Main.java
@@ -1,25 +1,23 @@
 import java.util.Scanner;
 
-class Solution {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         System.out.println(countDigits(n));
     }
-    static int countDigits(int n) {
 
-        int count=0;
-        if(n==0){
+    static int countDigits(int n) {
+        int count = 0;
+
+        if (n == 0) {
             return 1;
         }
-        while(n!=0){
-            n=n/10;
+
+        while (n != 0) {
+            n = n / 10;
             count++;
         }
+
         return count;
-
     }
-
-
-
-}
